e131502b1df50fa6006987bc7f565d7a287b416b
Akallabet <Akallabet@users.noreply.github.com>
2020-04-07 08:03:23 +0000

    deploy: f14e6f5f21c98513049763fbff33e14e8203e008

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-56815915033a293e9a4c.js"
+    "url": "webpack-runtime-8a10e0e765aa01319842.js"
   },
   {
     "url": "framework-9b7b0aa78083a4c4cab6.js"
@@ -43,7 +43,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "7f89a994911965fc67988d4273276a8e"
+    "revision": "d3ab475b439f29d9d98de7ad05a5ad63"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-8f20f2c7b8ac61d8df98.js"
@@ -54,7 +54,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "27326223be5ba87edba708070980237e"
+    "revision": "65c82a9d810f5528fc1a0fd0317c1c58"
   },
   {
     "url": "manifest.webmanifest",
